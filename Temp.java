9328efdfba5d576ae285dc04b4cbd2d605c8e870
Pooja Verma <pooja32verma@gmail.com>
2024-01-06 15:47:02 -0800

    Temperature converter using java

---
+++ Temp.java
@@ -0,0 +1,13 @@
+
+public class Temp {
+    public static void main(String[] args) {
+    int temp = 36;
+    float var = 9/5f;
+    float var2 = var;
+    int b =32;
+    float result = (var * temp) + b;
+    System.out.println(var2);
+    System.out.println(result);     
+    }
+   
+}
