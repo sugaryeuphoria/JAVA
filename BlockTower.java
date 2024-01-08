6d9eece6890513166bb059a1b5e7e9e3179c9cfc
Pooja Verma <pooja32verma@gmail.com>
2024-01-07 18:54:01 -0800

    addition in line 8

--- BlockTower.java
+++ BlockTower.java
@@ -5,6 +5,6 @@ public class BlockTower {
       System.out.println("Block added! Tower is now " + numBlocks + " blocks tall!");
       numBlocks++;  // Stack another block!
     }
-    System.out.println("Whoa! The tower is super tall now!");
+    System.out.println("Whoa! The tower is super tall!");
   }
 }
