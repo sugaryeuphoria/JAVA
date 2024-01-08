40a7079a5085f78e48ed56b127da00eadc43710c
Pooja Verma <pooja32verma@gmail.com>
2024-01-07 18:59:34 -0800

    Pallindrome :Determine if an integer is a palindrome (reads the same backward as forward). Leet Code

---
+++ Pallindrome.java
@@ -0,0 +1,10 @@
+public class Pallindrome {
+    public boolean isPalindrome(int x) {
+        int reversed = 0;
+        int original = x;
+    
+   
+        // If the reversed number is the same as the original, it's a palindrome!
+        return reversed == original;
+    }
+}
