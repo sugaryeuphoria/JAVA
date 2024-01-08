9823c4846e8315883b32d98334b42686c0c00070
Pooja Verma <pooja32verma@gmail.com>
2024-01-07 19:00:53 -0800

    Find the contiguous subarray with the largest sum. Leet code

---
+++ maxSubArray.java
@@ -0,0 +1,10 @@
+public class maxSubArray {
+    public int maxSubArray(int[] nums) {
+        int bestSum = nums[0]; // Start with the first number
+        int currentSum = nums[0]; // Keep track of the sum as we go
+    
+        
+    
+        return bestSum;
+    }
+}
