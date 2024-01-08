c2eecb130a6543bbc363f7d713f4f69f1130bd32
Pooja Verma <pooja32verma@gmail.com>
2024-01-07 18:58:15 -0800

    While block added

--- Reverse.java
+++ Reverse.java
@@ -4,7 +4,11 @@ public class Reverse {
         int remainder = 0; // Like, the leftover when you divide
         
         // Keep playing with the number until it's all used up
-       
+        while (x != 0) {
+            remainder = x % 10; // Get the last digit
+            reversed = reversed * 10 + remainder; // Stick it on the end
+            x /= 10; // Get rid of the last digit
+        }
     
         return reversed;
     }
