1d3e6041afb41754cb43ed51fa95040c1d392ce2
Pooja Verma <pooja32verma@gmail.com>
2024-01-07 18:55:46 -0800

    LeetCode Two Sum question

---
+++ TwoSum.java
@@ -0,0 +1,15 @@
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        // Like, look at each number and see if its buddy is in the list
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    // Yay, found the buddies!
+                    return new int[] {i, j};
+                }
+            }
+        }
+        
+    }
+    
+}
