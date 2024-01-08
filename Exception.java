b84fe163b873e8f25e9e72085fd82ba271e132c1
Pooja Verma <pooja32verma@gmail.com>
2024-01-07 19:05:16 -0800

    Made correction in line 9

--- Exception.java
+++ Exception.java
@@ -1,12 +1,16 @@
+import java.util.Arrays;
+
 public class Exception {
-    public static void main(String[] args) {
-        try {
-          int[] myNumbers = {1, 2};
-          System.out.println(myNumbers);
-        } catch (Exception e) {
-          System.out.println("Something went wrong.");
-        } finally {
-          System.out.println("The 'try catch' is finished.");
-        }
-      }
+  public static void main(String[] args) {
+     try {
+      int[] myNumbers = {1, 2};
+      // Use Arrays.toString() to print the array contents correctly:
+      System.out.println(Arrays.toString(myNumbers));
+    } catch (Exception e) {  // Keep the general catch block for unexpected errors
+      System.out.println("Something went wrong.");
+    
+    } finally {
+      System.out.println("The 'try catch' is finished.");
+    }
+  }
 }
