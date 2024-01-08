0954ef2f38aa2910e23d245d42a63a5aef0abe39
Pooja Verma <pooja32verma@gmail.com>
2024-01-07 18:57:49 -0800

    Reverse the digits of an integer. Leet Code

---
+++ Reverse.java
@@ -0,0 +1,11 @@
+public class Reverse {
+    public int reverse(int x) {
+        int reversed = 0;
+        int remainder = 0; // Like, the leftover when you divide
+        
+        // Keep playing with the number until it's all used up
+       
+    
+        return reversed;
+    }
+}
