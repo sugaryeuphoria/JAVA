a1c6572bf478e868af4a7c5816954121998c5075
Pooja Verma <pooja32verma@gmail.com>
2024-01-07 19:00:03 -0800

    While block added

--- Pallindrome.java
+++ Pallindrome.java
@@ -3,7 +3,11 @@ public class Pallindrome {
         int reversed = 0;
         int original = x;
     
-   
+    // Make a copy of the number in reverse
+    while (x != 0) {
+        reversed = reversed * 10 + x % 10;
+        x /= 10;
+    }
         // If the reversed number is the same as the original, it's a palindrome!
         return reversed == original;
     }
